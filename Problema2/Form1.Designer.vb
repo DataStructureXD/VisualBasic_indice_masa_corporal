e4225f198a029863101ec2bebaa8f738b0bb84bb
rubencerezo <ruben.chambers10@gmail.com>
2021-12-13 22:21:01 -0500

    Se envio correctamente

---
+++ Problema2/Form1.Designer.vb
@@ -0,0 +1,326 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.M = New System.Windows.Forms.RadioButton()
+        Me.F = New System.Windows.Forms.RadioButton()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtEstatura = New System.Windows.Forms.TextBox()
+        Me.txtPeso = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtApellido = New System.Windows.Forms.TextBox()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.txtCedula = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtIMC = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.txtEstado = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(108, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(429, 32)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "INDICE DE MASA CORPORAL"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.M)
+        Me.GroupBox1.Controls.Add(Me.F)
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Controls.Add(Me.txtEstatura)
+        Me.GroupBox1.Controls.Add(Me.txtPeso)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Location = New System.Drawing.Point(32, 199)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(377, 168)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Ingrese sus datos Personales:"
+        '
+        'M
+        '
+        Me.M.AutoSize = True
+        Me.M.Location = New System.Drawing.Point(136, 42)
+        Me.M.Name = "M"
+        Me.M.Size = New System.Drawing.Size(40, 21)
+        Me.M.TabIndex = 13
+        Me.M.TabStop = True
+        Me.M.Text = "M"
+        Me.M.UseVisualStyleBackColor = True
+        '
+        'F
+        '
+        Me.F.AutoSize = True
+        Me.F.Location = New System.Drawing.Point(93, 41)
+        Me.F.Name = "F"
+        Me.F.Size = New System.Drawing.Size(37, 21)
+        Me.F.TabIndex = 12
+        Me.F.TabStop = True
+        Me.F.Text = "F"
+        Me.F.UseVisualStyleBackColor = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(19, 42)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(67, 17)
+        Me.Label8.TabIndex = 11
+        Me.Label8.Text = "Genero:"
+        '
+        'txtEstatura
+        '
+        Me.txtEstatura.Location = New System.Drawing.Point(93, 123)
+        Me.txtEstatura.Name = "txtEstatura"
+        Me.txtEstatura.Size = New System.Drawing.Size(100, 22)
+        Me.txtEstatura.TabIndex = 10
+        Me.txtEstatura.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'txtPeso
+        '
+        Me.txtPeso.Location = New System.Drawing.Point(93, 71)
+        Me.txtPeso.Name = "txtPeso"
+        Me.txtPeso.Size = New System.Drawing.Size(100, 22)
+        Me.txtPeso.TabIndex = 9
+        Me.txtPeso.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(19, 126)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(74, 17)
+        Me.Label6.TabIndex = 4
+        Me.Label6.Text = "Estatura:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(19, 74)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(49, 17)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "Peso:"
+        '
+        'txtApellido
+        '
+        Me.txtApellido.Location = New System.Drawing.Point(117, 120)
+        Me.txtApellido.Name = "txtApellido"
+        Me.txtApellido.Size = New System.Drawing.Size(100, 22)
+        Me.txtApellido.TabIndex = 8
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(117, 79)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(100, 22)
+        Me.txtNombre.TabIndex = 7
+        '
+        'txtCedula
+        '
+        Me.txtCedula.Location = New System.Drawing.Point(117, 163)
+        Me.txtCedula.Name = "txtCedula"
+        Me.txtCedula.Size = New System.Drawing.Size(100, 22)
+        Me.txtCedula.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(40, 120)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(71, 17)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Apellido:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(40, 79)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(69, 17)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Nombre:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(44, 163)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(65, 18)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Cedula:"
+        '
+        'txtIMC
+        '
+        Me.txtIMC.Location = New System.Drawing.Point(130, 410)
+        Me.txtIMC.Multiline = True
+        Me.txtIMC.Name = "txtIMC"
+        Me.txtIMC.Size = New System.Drawing.Size(119, 35)
+        Me.txtIMC.TabIndex = 11
+        Me.txtIMC.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(47, 413)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(77, 32)
+        Me.Label7.TabIndex = 5
+        Me.Label7.Text = "IMC:"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(565, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "SALIR"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalcular.ForeColor = System.Drawing.Color.DarkSlateGray
+        Me.btnCalcular.Location = New System.Drawing.Point(323, 86)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(228, 78)
+        Me.btnCalcular.TabIndex = 3
+        Me.btnCalcular.Text = "Enviar Datos"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDelete.Location = New System.Drawing.Point(442, 242)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(161, 70)
+        Me.btnDelete.TabIndex = 4
+        Me.btnDelete.Text = "Limpiar"
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(312, 410)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(0, 17)
+        Me.Label9.TabIndex = 12
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(275, 413)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(161, 32)
+        Me.Label10.TabIndex = 13
+        Me.Label10.Text = "Su estado:"
+        '
+        'txtEstado
+        '
+        Me.txtEstado.Location = New System.Drawing.Point(442, 342)
+        Me.txtEstado.Multiline = True
+        Me.txtEstado.Name = "txtEstado"
+        Me.txtEstado.Size = New System.Drawing.Size(198, 136)
+        Me.txtEstado.TabIndex = 14
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(652, 490)
+        Me.Controls.Add(Me.txtEstado)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.txtIMC)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.txtApellido)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtCedula)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents txtIMC As System.Windows.Forms.TextBox
+    Friend WithEvents txtEstatura As System.Windows.Forms.TextBox
+    Friend WithEvents txtPeso As System.Windows.Forms.TextBox
+    Friend WithEvents txtApellido As System.Windows.Forms.TextBox
+    Friend WithEvents txtNombre As System.Windows.Forms.TextBox
+    Friend WithEvents txtCedula As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents btnCalcular As System.Windows.Forms.Button
+    Friend WithEvents btnDelete As System.Windows.Forms.Button
+    Friend WithEvents M As System.Windows.Forms.RadioButton
+    Friend WithEvents F As System.Windows.Forms.RadioButton
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents txtEstado As System.Windows.Forms.TextBox
+
+End Class
---
+++ Problema2/Form1.vb
@@ -0,0 +1,49 @@
+﻿Public Class Form1
+    Dim cedula As Integer
+    Dim nombre, apellido, estado As String
+    Dim peso, Imc As Single
+    Dim altura As Double
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        End
+
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        txtApellido.Text = ""
+        txtCedula.Text = ""
+        txtEstatura.Text = ""
+        txtIMC.Text = ""
+        txtNombre.Text = ""
+        txtPeso.Text = ""
+        txtEstado.Text = ""
+    End Sub
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        Try
+            cedula = CDbl(Me.txtCedula.Text)
+            peso = CDbl(Me.txtPeso.Text)
+            altura = CDbl(Me.txtEstatura.Text)
+        Catch ex As Exception
+            MsgBox("TEXTO NO DETECTADO, TIENES QUE ESCRIBIR UN NUMERO")
+        End Try
+
+        txtIMC.Text = Val(txtPeso.Text) / Val(txtEstatura.Text ^ 2)
+
+        Select Case (txtIMC.Text)
+            Case Is <= 14.9
+                txtEstado.Text = "Normal"
+
+            Case Is >= 18
+                txtEstado.Text = "Sobre peso"
+
+            Case Is >= 25
+                txtEstado.Text = "Excesivamente"
+
+            Case Is >= 34
+                txtEstado.Text = "Pasado de peso"
+
+            Case Else
+                txtEstado.Text = " Usted esta en buenas Condiciones "
+        End Select
+
+    End Sub
+End Class
